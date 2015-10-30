1af5892d6328ae9fa26adea1cfd45a37c7b662cd
nikitafomin <nikitafomin89@gmail.com>
2015-10-30 17:55:39 +0300

    Add ItemsListViewController with collection view

---
+++ Swift-Base/ItemListCollectionViewCell.swift
@@ -0,0 +1,13 @@
+//
+//  ItemListCollectionViewCell.swift
+//  Swift-Base
+//
+//  Created by Nikita Fomin on 30.10.15.
+//  Copyright © 2015 Flatstack. All rights reserved.
+//
+
+import UIKit
+
+class ItemListCollectionViewCell: UICollectionViewCell {
+    
+}
---
+++ Swift-Base/ItemsListViewController.swift
@@ -0,0 +1,38 @@
+//
+//  ItemsListViewController.swift
+//  Swift-Base
+//
+//  Created by Nikita Fomin on 30.10.15.
+//  Copyright © 2015 Flatstack. All rights reserved.
+//
+
+import UIKit
+
+class ItemsListViewController: UIViewController {
+
+    @IBOutlet weak var searchBar: UISearchBar!
+    @IBOutlet weak var collectionView: UICollectionView!
+    
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepareForSegue(segue: UIStoryboardSegue, sender: AnyObject?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
