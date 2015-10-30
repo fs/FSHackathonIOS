d65476db3fac65ae52230024a7d4aff805622e13
nikitafomin <nikitafomin89@gmail.com>
2015-10-30 13:47:35 +0300

    Add detail controller and cell

---
+++ Swift-Base/DetailItemViewCell.swift
@@ -0,0 +1,24 @@
+//
+//  DetailItemViewCell.swift
+//  Swift-Base
+//
+//  Created by Nikita Fomin on 30.10.15.
+//  Copyright © 2015 Flatstack. All rights reserved.
+//
+
+import UIKit
+
+class DetailItemViewCell: UITableViewCell {
+
+    override func awakeFromNib() {
+        super.awakeFromNib()
+        // Initialization code
+    }
+
+    override func setSelected(selected: Bool, animated: Bool) {
+        super.setSelected(selected, animated: animated)
+
+        // Configure the view for the selected state
+    }
+
+}
