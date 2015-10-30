312b1977af354eddcbb1dda5355a9e5303416ac5
Aynur Galiev <aynur.galiev@flatstack.com>
2015-10-30 13:30:49 +0300

    Updating bridge header

--- Swift-Base/BridgeHeader.h
+++ Swift-Base/BridgeHeader.h
@@ -6,9 +6,4 @@
 //  Copyright (c) 2014 Flatstack. All rights reserved.
 //
 
-#import "AFNetworking.h"
-//#import <MagicalRecord.h>
-//#import "UIKit+AFNetworking.h"
-//#import "SDWebImage/UIImageView+WebCache.h"
-//#import "SDWebImage/SDImageCache.h"
-//#import "NSDate-Utilities.h"
+#import <MagicalRecord.h>
