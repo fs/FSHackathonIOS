8d753870c09c518c621426e268ec5ea25e79d182
nikitafomin <nikitafomin89@gmail.com>
2015-10-30 16:28:47 +0300

    Setup prepare cell for Detail list cell

--- Swift-Base/DetailItemViewCell.swift
+++ Swift-Base/DetailItemViewCell.swift
@@ -25,10 +25,14 @@ class DetailItemViewCell: UITableViewCell {
         // Configure the view for the selected state
     }
     
-//    func prepareCell(listedItem: ListedItem) {
-////        self.customImageView.image = item.image
-//        self.titleLabel.text = item.title
-////        self.detailLabel.text = item.title
-//    }
+    func prepareCell(listedItem: ListedItem) {
+        self.customImageView.image = UIImage(named: (listedItem.item?.title)!)
+        self.titleLabel.text = listedItem.item?.title
+        
+        // detail
+        let count = "\((listedItem.count?.doubleValue)!) \(listedItem.unit)"
+        let price = "\((listedItem.count?.doubleValue)! * (listedItem.item?.minPrice?.doubleValue)!)"
+        self.detailLabel.text = "Количество: \(count), примерная стоимость: \(price)"
+    }
     
 }
