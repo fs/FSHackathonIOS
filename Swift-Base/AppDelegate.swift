7ef01dd03ce542cc2fd113e30692c8e0e994d1ba
nikitafomin <nikitafomin89@gmail.com>
2015-10-30 19:35:09 +0300

    Add filling DB

--- Swift-Base/AppDelegate.swift
+++ Swift-Base/AppDelegate.swift
@@ -12,34 +12,207 @@ import Crashlytics
 
 @UIApplicationMain
 class AppDelegate: UIResponder, UIApplicationDelegate {
-
+    
     var window: UIWindow?
-
-    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool {
-        
-        self.setupLibraries()
-        
-        for i in 0 ... 5 {
-            let list = List.MR_createEntityInContext(NSManagedObjectContext.MR_defaultContext())
-            list.title = "Test \(i + 1)"
-        }
+    
+    func application(application: UIApplication, didFinishLaunchingWithOptions launchOptions: [NSObject: AnyObject]?) -> Bool
+    {
+        self.setupMagicalRecords();
+        self.fillDB()
         
         return true
     }
-
+    
+    func fillDB() {
+        if Item.MR_findAll()!.count == 0 {
+            
+            //Фрукты
+            let item1 = Item.MR_createEntity()
+            item1!.title = "Яблоки"
+            item1!.category = "Фрукты"
+            item1!.minPrice = NSNumber(float: 55.0)
+            item1!.maxPrice = NSNumber(float: 71.0)
+            
+            let item2 = Item.MR_createEntity()
+            item2!.title = "Виноград"
+            item2!.category = "Фрукты"
+            item2!.minPrice = NSNumber(float: 112.0)
+            item2!.maxPrice = NSNumber(float: 144.0)
+            
+            let item3 = Item.MR_createEntity()
+            item3!.title = "Персики"
+            item3!.category = "Фрукты"
+            item3!.minPrice = NSNumber(float: 81.0)
+            item3!.maxPrice = NSNumber(float: 109.0)
+            
+            let item4 = Item.MR_createEntity()
+            item4!.title = "Груши"
+            item4!.category = "Фрукты"
+            item4!.minPrice = NSNumber(float: 66.0)
+            item4!.maxPrice = NSNumber(float: 79.0)
+            
+            //Овощи
+            let item5 = Item.MR_createEntity()
+            item5!.title = "Помидоры"
+            item5!.category = "Овощи"
+            item5!.minPrice = NSNumber(float: 140.0)
+            item5!.maxPrice = NSNumber(float: 155.0)
+            
+            let item6 = Item.MR_createEntity()
+            item6!.title = "Огурцы"
+            item6!.category = "Овощи"
+            item6!.minPrice = NSNumber(float: 120.0)
+            item6!.maxPrice = NSNumber(float: 132.0)
+            
+            //Яйца и молочные продукты
+            let item7 = Item.MR_createEntity()
+            item7!.title = "Молоко"
+            item7!.category = "Яйца и молочные продукты"
+            item7!.minPrice = NSNumber(float: 29.0)
+            item7!.maxPrice = NSNumber(float: 61.0)
+            
+            let item8 = Item.MR_createEntity()
+            item8!.title = "Йогурт"
+            item8!.category = "Яйца и молочные продукты"
+            item8!.minPrice = NSNumber(float: 14)
+            item8!.maxPrice = NSNumber(float: 29.0)
+            
+            
+            let item9 = Item.MR_createEntity()
+            item9!.title = "Кефир"
+            item9!.category = "Яйца и молочные продукты"
+            item9!.minPrice = NSNumber(float: 14.0)
+            item9!.maxPrice = NSNumber(float: 27.0)
+            
+            let item10 = Item.MR_createEntity()
+            item10!.title = "Катык"
+            item10!.category = "Яйца и молочные продукты"
+            item10!.minPrice = NSNumber(float: 15.0)
+            item10!.maxPrice = NSNumber(float: 26.0)
+            
+            //Мука и мучные изделия
+            let item11 = Item.MR_createEntity()
+            item11!.title = "Хлеб"
+            item11!.category = "Мука и мучные изделия"
+            item11!.minPrice = NSNumber(float: 15.0)
+            item11!.maxPrice = NSNumber(float: 44.0)
+            
+            let item12 = Item.MR_createEntity()
+            item12!.title = "Батон"
+            item12!.category = "Мука и мучные изделия"
+            item12!.minPrice = NSNumber(float: 15.0)
+            item12!.maxPrice = NSNumber(float: 32.0)
+            
+            let item13 = Item.MR_createEntity()
+            item13!.title = "Мука"
+            item13!.category = "Мука и мучные изделия"
+            item13!.minPrice = NSNumber(float: 56.0)
+            item13!.maxPrice = NSNumber(float: 78.0)
+            
+            //Кондитерские изделия
+            let item14 = Item.MR_createEntity()
+            item14!.title = "Шоколад"
+            item14!.category = "Кондитерские изделия"
+            item14!.minPrice = NSNumber(float: 56.0)
+            item14!.maxPrice = NSNumber(float: 98.0)
+            
+            let item15 = Item.MR_createEntity()
+            item15!.title = "Конфеты"
+            item15!.category = "Кондитерские изделия"
+            item15!.minPrice = NSNumber(float: 152.0)
+            item15!.maxPrice = NSNumber(float: 389.0)
+            
+            let item16 = Item.MR_createEntity()
+            item16!.title = "Торты"
+            item16!.category = "Кондитерские изделия"
+            item16!.minPrice = NSNumber(float: 120.0)
+            item16!.maxPrice = NSNumber(float: 465.0)
+            
+            //Бакалея
+            let item17 = Item.MR_createEntity()
+            item17!.title = "Мед"
+            item17!.category = "Бакалея"
+            item17!.minPrice = NSNumber(float: 205.0)
+            item17!.maxPrice = NSNumber(float: 604.0)
+            
+            let item18 = Item.MR_createEntity()
+            item18!.title = "Майонез"
+            item18!.category = "Бакалея"
+            item18!.minPrice = NSNumber(float: 27.0)
+            item18!.maxPrice = NSNumber(float: 67.0)
+            
+            let item19 = Item.MR_createEntity()
+            item19!.title = "Масло растительное"
+            item19!.category = "Бакалея"
+            item19!.minPrice = NSNumber(float: 51.0)
+            item19!.maxPrice = NSNumber(float: 88.0)
+            
+            //Мясные продукты
+            let item20 = Item.MR_createEntity()
+            item20!.title = "Колбаса"
+            item20!.category = "Мясные продукты"
+            item20!.minPrice = NSNumber(float: 154.0)
+            item20!.maxPrice = NSNumber(float: 879.0)
+            
+            let item21 = Item.MR_createEntity()
+            item21!.title = "Сосиски"
+            item21!.category = "Мясные продукты"
+            item21!.minPrice = NSNumber(float: 102.0)
+            item21!.maxPrice = NSNumber(float: 165.0)
+            
+            let item22 = Item.MR_createEntity()
+            item22!.title = "Сало"
+            item22!.category = "Мясные продукты"
+            item22!.minPrice = NSNumber(float: 77.0)
+            item22!.maxPrice = NSNumber(float: 261.0)
+            
+            //Рыба
+            let item23 = Item.MR_createEntity()
+            item23!.title = "Рыба соленая"
+            item23!.category = "Рыба"
+            item23!.minPrice = NSNumber(float: 155.0)
+            item23!.maxPrice = NSNumber(float: 265.0)
+            
+            let item24 = Item.MR_createEntity()
+            item24!.title = "Икра"
+            item24!.category = "Рыба"
+            item24!.minPrice = NSNumber(float: 402.0)
+            item24!.maxPrice = NSNumber(float: 1230.0)
+            
+            let list = List.MR_createEntity()
+            list!.title = "New List"
+            list!.date = NSDate()
+            
+            let listedItem1 = ListedItem.MR_createEntity()
+            listedItem1!.count =  NSNumber(float: 2.0)
+            listedItem1!.unit = Unit.Kilogramm.numberValue
+            listedItem1!.item = item13;
+            
+            let listedItem2 = ListedItem.MR_createEntity()
+            listedItem2!.count =  NSNumber(float: 2.0)
+            listedItem2!.unit = Unit.Gramm.numberValue
+            listedItem2!.item = item15;
+            
+            let listedItem3 = ListedItem.MR_createEntity()
+            listedItem3!.count =  NSNumber(float: 2.0)
+            listedItem3!.unit = Unit.Count.numberValue
+            listedItem3!.item = item11;
+            
+            list!.listedItem = NSSet(array: [listedItem1!, listedItem2!, listedItem3!])
+            
+            NSManagedObjectContext.MR_defaultContext().MR_saveToPersistentStoreAndWait()
+        }
+    }
+    
 }
+
 
 //MARK: - Setup Services and Libraries
 extension AppDelegate {
-    private func setupLibraries () {
-        //Setting MagicalRecord
-        self.setupMagicalRecords()
-    }
-    
     private func setupMagicalRecords () {
+        MagicalRecord.setupCoreDataStack()
         MagicalRecord.setShouldDeleteStoreOnModelMismatch(true)
         MagicalRecord.setupAutoMigratingCoreDataStack()
         MagicalRecord.setLoggingLevel(.Off)
     }
 }
-
--- Swift-Base/Constants.swift
+++ Swift-Base/Constants.swift
@@ -52,3 +52,45 @@ enum FontFamilyName {
 //Example of custom color schemes in application
 let MyRedApplicationColor = UIColor.redColor()
 let MyGreenApplicationColor = UIColor.greenColor()
+
+enum Unit: String {
+    
+    case Kilogramm = "Масса"
+    case Volume = "Объем"
+    case Count = "Штука"
+    case Gramm = "Грамм"
+    case Undefined = ""
+    
+    var short: String {
+        
+        switch self {
+        case .Kilogramm   : return "кг"
+        case .Volume      : return "л"
+        case .Count       : return "шт"
+        case .Gramm       : return "гр"
+        case .Undefined   : return ""
+        }
+    }
+    
+    init?(intValue: Int16) {
+        
+        switch intValue {
+        case 0: self = .Kilogramm
+        case 1: self = .Volume
+        case 2: self = .Count
+        case 3: self = .Gramm
+        default: self = .Undefined
+        }
+    }
+    
+    var numberValue: NSNumber {
+        
+        switch self {
+        case .Kilogramm   : return NSNumber(float: 0.0)
+        case .Volume      : return NSNumber(float: 1.0)
+        case .Count       : return NSNumber(float: 2.0)
+        case .Gramm       : return NSNumber(float: 3.0)
+        case .Undefined   : return NSNumber(float: -1.0)
+        }
+    }
+}
